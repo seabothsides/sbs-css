d4dfbc2bbf1b7c771da27aa7f832e6d7e1aea26a
seabothsides <youngserge1@gmail.com>
2022-02-12 02:18:16 +0000

    buttons folder added

---
+++ components/buttons/hoverMenu.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="group bg-red-300 flex flex-row space-x-2 h-6 w-8">
+      <div id="trigger">trigger</div>
+      <div class="opacity-0 z-30 group-hover:opacity-100 duration-200">
+        <ul>
+          <li>links</li>
+          <li>links</li>
+          <li>links</li>
+          <li>links</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#result {
+  display: none;
+}
+#trigger:hover + #result {
+  display: block;
+}
+</style>
--- pages/navs/index.vue
+++ pages/navs/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="grid grid-flow-row gap-4">
+  <div class="grid grid-flow-row gap-5">
     <navs-basic />
     <navs-center />
+    <buttons-hover-menu />
   </div>
 </template>
